bea2a859ceb8b095fed906ccf1cda705e3214e76
GrigoryanDav <grigoryand219@gmail.com>
2024-11-27 18:39:27 +0400

    Implement CryptoList component with Ant Design table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import CryptoList from './pages/cryptoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <CryptoList />
     </div>
   );
 }
---
+++ src/pages/cryptoList/index.tsx
@@ -0,0 +1,68 @@
+import { requestUrls } from "../../util/constants/requestUrls";
+import { useFetch } from "../../hooks/useFetch";
+import { Table } from "antd";
+import type { TableProps } from "antd";
+import { CurrencyResponseModel } from "../../ts/types/CurrencyResponseModel";
+
+
+const CryptoList = () => {
+    const { data, loading, error } = useFetch<CurrencyResponseModel[]>({
+        url: `${requestUrls.coinsMarkets}?vs_currency=usd`
+    })
+
+
+    const columns: TableProps<CurrencyResponseModel>['columns'] = [
+        {
+            title: '#ID',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'Image',
+            dataIndex: 'image',
+            key: 'image',
+            render: (value) => {
+                return (
+                    <img src={value} width={50} height={50} />
+                )
+            }
+        },
+        {
+            title: 'name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Price Change 24',
+            dataIndex: 'price_change_24h',
+            key: 'price_change_24h,'
+        },
+        {
+            title: 'Price',
+            dataIndex: 'current_price',
+            key: 'current_price',
+        }
+    ]
+
+    const handleNavigateDetailPage = (row: CurrencyResponseModel) => {
+        console.log(row.id, '>>>>')
+    }
+
+    return (
+        <div>
+            <Table 
+                columns={columns}
+                loading={loading}
+                dataSource={data || []}
+                onRow={(row) => {
+                    return {
+                        onClick: () => handleNavigateDetailPage(row)
+                    }
+                }}
+            />
+        </div>
+    )
+}
+
+
+export default CryptoList
