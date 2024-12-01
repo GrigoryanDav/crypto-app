6310ac3bc97f6cfb055fd9e66d412fb01ab2a517
GrigoryanDav <grigoryand219@gmail.com>
2024-12-01 18:46:58 +0400

    Add MainLayout component and add routing for it

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CryptoDetail from './pages/cryptoDetail';
 import CryptoList from './pages/cryptoList';
 import { ROUTE_PATHS } from './util/constants/routes';
+import MainLayout from './components/layouts/Main';
 import './App.css';
 
 function App() {
@@ -9,8 +10,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={ROUTE_PATHS.HOME} element={<CryptoList />} />
-          <Route path={`${ROUTE_PATHS.CRYPTO_DETAIL}/:id`} element={<CryptoDetail />} />
+          <Route path={ROUTE_PATHS.HOME} element={<MainLayout />}>
+            <Route path={ROUTE_PATHS.HOME} element={<CryptoList />} />
+            <Route path={`${ROUTE_PATHS.CRYPTO_DETAIL}/:id`} element={<CryptoDetail />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
